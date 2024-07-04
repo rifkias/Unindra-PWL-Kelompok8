9b49100adf749b109d63e57b9cd10633df62712e
Rifki Alfariz Shidiq <rifki.alfariz@lincgrp.com>
2024-07-04 09:52:56 +0700

    update assets link

--- index.php
+++ index.php
@@ -19,23 +19,23 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?= $uri.'assets/plugins/fontawesome-free/css/all.min.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/plugins/fontawesome-free/css/all.min.css'?>">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="<?= $uri.'assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'?>">
     <!-- iCheck -->
-    <link rel="stylesheet" href="<?= $uri.'assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css'?>">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="<?= $uri.'assets/plugins/jqvmap/jqvmap.min.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/plugins/jqvmap/jqvmap.min.css'?>">
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?= $uri.'assets/css/adminlte.min.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/css/adminlte.min.css'?>">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?= $uri.'assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'?>">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?= $uri.'assets/plugins/daterangepicker/daterangepicker.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/plugins/daterangepicker/daterangepicker.css'?>">
     <!-- summernote -->
-    <link rel="stylesheet" href="<?= $uri.'assets/plugins/summernote/summernote-bs4.min.css'?>">
+    <link rel="stylesheet" href="<?= $uri.'/assets/plugins/summernote/summernote-bs4.min.css'?>">
 </head>
 <?php 
     if(!$isAuth){
@@ -55,7 +55,7 @@
     <div class="wrapper">
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="<?= $uri.'assets/img/AdminLTELogo.png'?>" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="<?= $uri.'/assets/img/AdminLTELogo.png'?>" alt="AdminLTELogo" height="60" width="60">
         </div>
     </div>
         
@@ -77,39 +77,39 @@
     ?>
 
     <!-- jQuery -->
-    <script src="<?= $uri.'assets/plugins/jquery/jquery.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/jquery/jquery.min.js'?>"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="<?= $uri.'assets/plugins/jquery-ui/jquery-ui.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/jquery-ui/jquery-ui.min.js'?>"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="<?= $uri.'assets/plugins/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
     <!-- ChartJS -->
-    <script src="<?= $uri.'assets/plugins/chart.js/Chart.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/chart.js/Chart.min.js'?>"></script>
     <!-- Sparklin -->
-    <script src="<?= $uri.'assets/plugins/sparklines/sparkline.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/sparklines/sparkline.js'?>"></script>
     <!-- JQVMap -->
-    <script src="<?= $uri.'assets/plugins/jqvmap/jquery.vmap.min.js'?>"></script>
-    <script src="<?= $uri.'assets/plugins/jqvmap/maps/jquery.vmap.usa.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/jqvmap/jquery.vmap.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/jqvmap/maps/jquery.vmap.usa.js'?>"></script>
     <!-- jQuery Knob Chart -->
-    <script src="<?= $uri.'assets/plugins/jquery-knob/jquery.knob.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/jquery-knob/jquery.knob.min.js'?>"></script>
     <!-- daterangepicker -->
-    <script src="<?= $uri.'assets/plugins/moment/moment.min.js'?>"></script>
-    <script src="<?= $uri.'assets/plugins/daterangepicker/daterangepicker.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/moment/moment.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/daterangepicker/daterangepicker.js'?>"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="<?= $uri.'assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'?>"></script>
     <!-- Summernote -->
-    <script src="<?= $uri.'assets/plugins/summernote/summernote-bs4.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/summernote/summernote-bs4.min.js'?>"></script>
     <!-- overlayScrollbars -->
-    <script src="<?= $uri.'assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'?>"></script>
+    <script src="<?= $uri.'/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'?>"></script>
     <!-- AdminLTE App -->
-    <script src="<?= $uri.'assets/js/adminlte.js'?>"></script>
+    <script src="<?= $uri.'/assets/js/adminlte.js'?>"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="<?= $uri.'assets/js/demo.js'?>"></script>
+    <script src="<?= $uri.'/assets/js/demo.js'?>"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="<?= $uri.'assets/js/pages/dashboard.js'?>"></script>
+    <script src="<?= $uri.'/assets/js/pages/dashboard.js'?>"></script>
 </body>
 <?php 
     }else{
