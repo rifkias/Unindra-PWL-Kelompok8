29595315167177788e6f751d4c9010069981cfd0
Rifki Alfariz Shidiq <rifki.alfariz@lincgrp.com>
2024-07-05 00:04:08 +0700

    location pagination

--- page/lokasi/LocationController.php
+++ page/lokasi/LocationController.php
@@ -2,15 +2,65 @@
 // include('config/koneksi.php');
 class LocationController {
     protected $koneksi = null;
-
+    protected $perPage = 10;
+    protected $page = 1;
     public function __construct($conn)
     {
         $this->koneksi = $conn;
     }
 
-    public function getData(){
-        $query = "SELECT * FROM location";
+    public function getData($params){
+        if(@$params['perPage']){
+            $this->perPage = $params['perPage'];
+        }
+        if(@$params['page']){
+            $this->page = $params['page'];
+        }
+        $perPage = $this->perPage;
+        $page = $this->page;
+
+        $currentLimit =  ($page > 1) ? ($page * $perPage) - $perPage : "0";
+        $query = "SELECT * FROM location limit $currentLimit , $perPage";
         $res = $this->koneksi->query($query);
+
         return $res;
     }
+
+    public function getPagination(){
+        $res = [];
+        $countRow = $this->countData();
+        $res['currentNumber'] = ($this->page > 1) ? ($this->page * $this->perPage) - $this->perPage : "0";
+        $res['totalRow'] = $countRow;
+        $res['totalPage'] = ceil($countRow/$this->perPage);
+        $res['currentPage'] = $this->page;
+        $res['params'] = $this->getParameter();
+        return $res;
+    }
+
+    function getParameter(){
+        $uri = $_SERVER["QUERY_STRING"];
+        parse_str($uri, $queryArray);
+
+        if(count($queryArray) > 0){
+            $removedParams = "";
+            foreach($queryArray as $key => $val){
+                if($key !== 'page'){
+                    if($removedParams == ""){
+                        $removedParams .= "?".$key."=".$val;
+                    }else{
+                        $removedParams .= "&".$key."=".$val;
+                    }
+                }
+            }
+            $uri = $removedParams;
+        }
+        return $uri;
+    }
+    public function countData(){
+        $query = "SELECT * FROM location";
+        $res = $this->koneksi->query($query);
+        $total = $res->num_rows;
+        return $total;
+    }
+
 }
--- page/lokasi/index.php
+++ page/lokasi/index.php
@@ -1,7 +1,7 @@
 <?php
 require('LocationController.php');
 $controller = new LocationController($con);
-$data = $controller->getData();
+$data = $controller->getData($_GET);
 ?>
 
 <section class="content-header">
@@ -23,6 +23,63 @@ $data = $controller->getData();
 <!-- Main content -->
 <section class="content">
     <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Search</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>Nama Lokasi</label>
+                                        <input type="text" name="nama" class="form-control" value="<?= @$_GET['nama'] ?>" placeholder="Location Name">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>Nama Provinsi</label>
+                                        <input type="text" name="provinsi" class="form-control" value="<?= @$_GET['provinsi'] ?>" placeholder="Provinsi">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>Nama Kota</label>
+                                        <input type="text" name="city" class="form-control" value="<?= @$_GET['city'] ?>" placeholder="Kota">
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="form-group">
+                                        <label>Perpage</label>
+                                        <select name="perPage" id="" class="form-control">
+                                            <option <?= @$_GET['perPage'] == '2' ? "selected" : ""  ?> value="2">2</option>
+                                            <option <?= @$_GET['perPage'] == '10' ? "selected" : ""  ?> value="10">10</option>
+                                            <option <?= @$_GET['perPage'] == '20' ? "selected" : ""  ?> value="20">20</option>
+                                            <option <?= @$_GET['perPage'] == '30' ? "selected" : ""  ?> value="30">30</option>
+                                            <option <?= @$_GET['perPage'] == '40' ? "selected" : ""  ?> value="40">40</option>
+                                            <option <?= @$_GET['perPage'] == '50' ? "selected" : ""  ?> value="50">50</option>
+                                            <option <?= @$_GET['perPage'] == '100' ? "selected" : ""  ?> value="100">100</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <button type="submit" class="btn btn-primary float-right">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-12">
                 <div class="card">
@@ -54,7 +111,7 @@ $data = $controller->getData();
                                         <td><?= $d['sub_district'] ?></td>
                                         <td><?= $d['address_1'] ?></td>
                                         <td>
-                                            <button class="btn btn-sm btn-primary mr-1">Detail</button>
+                                            <a href="<?= $uri . '/lokasi/detail/' . $d['location_id'] ?>"><button class="btn btn-sm btn-primary mr-1">Detail</button></a>
                                             <button class="btn btn-sm btn-warning mr-1">Edit</button>
                                             <button class="btn btn-sm btn-danger mr-1">Delete</button>
                                         </td>
@@ -65,6 +122,22 @@ $data = $controller->getData();
                                 ?>
                             </tbody>
                         </table>
+                    </div>
+                    <div class="card-footer clearfix">
+                        <?php
+                        $paginateData = $controller->getPagination();
+                        ?>
+                        <ul class="pagination pagination-sm m-0 float-right">
+                            <li class="page-item <?= $paginateData['currentPage'] == '1' ? "disabled" : "" ?> "><a class="page-link" href="<?= $paginateData['params'] . "&page=" . $paginateData['currentPage'] - 1 ?>">«</a></li>
+                            <?php
+                            for ($x = 1; $x <= $paginateData['totalPage']; $x++) {
+                            ?>
+                            <li class="page-item <?=  $paginateData['currentPage'] == $x ? "disabled" : "" ?> "> <a class="page-link" href="<?= $paginateData['params'] . "&page=" . $x ?>"><?= $x ?></a> </li>
+                            <?php
+                            }
+                            ?>
+                            <li class="page-item <?= $paginateData['currentPage'] == $paginateData['totalPage'] ? "disabled" : "" ?>"><a class="page-link" href="<?= $paginateData['params'] . "&page=" . $paginateData['currentPage'] + 1 ?>">»</a></li>
+                        </ul>
                     </div>
                     <!-- /.card-body -->
                 </div>
